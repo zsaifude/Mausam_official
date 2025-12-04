3253553eadc4a887de74e24134c0f7fd00672f4a
zsaifude <zsaifude@gmail.com>
2025-12-04 10:19:31 -0500

    Inject weather emoji

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-  console.log(response.data);
-
+  iconElement.innerHTML = ` <img src="${response.data.condition.icon_url}" class="weather-emoji"/>`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
