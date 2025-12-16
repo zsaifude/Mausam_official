a43afce59efffe14537e4b72add99e9fec7a5e34
zsaifude <zsaifude@gmail.com>
2025-12-16 13:51:58 -0500

    Updated CSS and JS

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  return `${day}, ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
